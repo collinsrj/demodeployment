20c7e867be3d15d6c7cfcc5aca37e33ab5867d3c
Robert Collins <collinsrj@hotmail.com>
2025-03-22 10:13:31 +0000

    first commit

---
+++ src/main/java/com/collinsrj/demodeployment/DemodeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.collinsrj.demodeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collinsrj/demodeployment/DemodeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collinsrj.demodeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
